bc56c91835d8bb3f592386fafba52de33e8e5ef7
Ryan Kohnenkamp <rkohnenkamp@gmail.com>
2014-06-11 23:14:51 -0400

    Changed Admin/user models to accept changes in database migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 	has_many :grades, dependent: :destroy
 	has_many :videos, through: :viewings, dependent: :destroy
 
-	validates :name, presence: true, length: {maximum: 40}
+	validates :firstName, presence: true, length: {maximum: 40}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
 				uniqueness: {case_sensitive: false}
